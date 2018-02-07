329fca38f5d395831dc60d89093ad3c8e12f33e5
SungBinPark <psb2942@gmail.com>
2018-02-07 18:05:57 +0900

    Revert "Initial Commit"
    This reverts commit 3e7d36e4c3b88c6360bbb268cf3fa095c8be8a4e.

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Unit
+{
+    private const int invenSize = 12;
+
+    protected int level;
+    protected int number;
+    public int Level { get { return level; } }
+    public void changeLevel(int delta)
+    {
+        level += delta;
+    }
+    
+   
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit {
+
+    private const int invenSize = 12;
+
+    private int mp;
+    private int hungry;
+    private int [] itemList = new int [invenSize];
+    private int equippedItem;
+
+    public int Mp { get { return mp; } }
+    public int Hungry { get { return hungry; } }
+    public int [] ItemList { get { return itemList; } }
+    public int EquippedItem { get { return equippedItem; } }
+
+    private void Start()
+    {
+        attack = 1;
+        defense = 1;
+        hp = 120;
+        mp = 120;
+        hungry = 100;
+    }
+    public void changeMp(int delta)
+    {
+        mp += delta;
+    }
+
+    public void changeHungry(int delta)
+    {
+        hungry += delta;
+    }
+
+}
