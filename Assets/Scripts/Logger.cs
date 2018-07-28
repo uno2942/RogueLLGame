cdc37b169aa54ae3b9de7f229f6f07d631599fcf
incdux7450 <incdux7450@postech.ac.kr>
2018-07-28 15:39:13 +0900

    정현님이 전에 작성하신 logger를 업로드합니다.

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Logger : MonoBehaviour {
+
+    public List<string> LogList;
+    public Text logger;
+
+	// Use this for initialization
+	void Start () {
+        LogList = new List<string>();
+	}
+	
+	// Update is called once per frame
+	void AddLog (string _log) {
+        string log = logger.text;
+        log += string.Format("/n{0}", _log);
+	}
+
+    void Update() { }
+}
