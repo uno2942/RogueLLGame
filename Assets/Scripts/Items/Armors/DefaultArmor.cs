e55fd4fd58988697805a4b35c1943eb9401b0a83
incdux7450 <incdux7450@gmail.com>
2018-08-26 19:14:09 +0900

    밸패:기본 방어령 0~1로 수정

--- Assets/Scripts/Items/Armors/DefaultArmor.cs
+++ Assets/Scripts/Items/Armors/DefaultArmor.cs
@@ -6,6 +6,7 @@ public class DefaultArmor : Armor {
     public DefaultArmor() {
         name = this.GetType().ToString();
         defensivePowerMin = 0 ;
+        defensivePowerMax = 1;
         rank = ItemManager.Rank.Common;
         }
 }
